77cb76014d3697822e33eb705da0ef51d219442a
daosuong <suong.dao123@hcmut.edu.vn>
2022-05-31 16:06:28 +0700

    12th commit

--- src/component/DetailContent.js
+++ src/component/DetailContent.js
@@ -283,6 +283,63 @@ function AddWH() {
 };
 
 function DetailLocation() {
+  const [data, setData] = useState(detailData);
+  const handleDelete = (index,e) => {
+      setData(data.filter((v, i) => i !== index));
+  }
+  const rows = data.map((item, index) => {
+    return (
+        <tr key={index}>
+          <td class="sm:p-3 py-2 px-1 border-b border-gray-200">
+            <div class="flex items-center">{item.id}</div>
+          </td>
+          <td class="sm:p-3 py-2 px-1 border-b border-gray-200">
+            <div class="flex items-center">
+              {item.name}
+            </div>
+          </td>
+          <td class="sm:p-3 py-2 px-1 border-b border-gray-200 md:table-cell hidden">
+            <div class="flex items-center">
+              {item.type === "Light" ? <Image path={`https://svgshare.com/i/hj5.svg`} className="w-4 h-4 mr-2.5"/> : item.type === "Cooler" ? <Image path={`https://svgshare.com/i/hoA.svg`} className="w-4 h-4 mr-2.5"/>: <Image path={`https://svgshare.com/i/hnH.svg`} className="w-4 h-4 mr-2.5"/>}
+              {item.type}
+            </div>
+          </td>
+          {item.state ? <td class="sm:p-3 py-2 px-1 border-b border-gray-200 text-green-600">On</td> : <td class="sm:p-3 py-2 px-1 border-b border-gray-200 text-red-600">Off</td>}
+          <td class="sm:p-3 py-2 px-1 border-b border-gray-200 md:table-cell hidden">
+            <div class="flex items-center">
+              {item.fInfo}
+            </div>
+          </td>
+          <td class="sm:p-3 py-2 px-1 border-b border-gray-200">
+            <div class="flex items-center">
+              <div class="sm:flex hidden flex-col">
+                {item.date}
+                <div class="text-gray-400 text-xs">{item.time}</div>
+              </div>
+              {/* <button class="w-4 h-4 inline-flex items-center justify-center text-gray-400 ml-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                </svg>
+              </button> */}
+              <EditE
+                id={item.id}
+                name={item.name}
+                type={item.type}
+                state={item.state}
+                location={item.location}
+                fInfo={item.fInfo}
+              />
+              <button class="w-4 h-4 inline-flex items-center justify-center text-gray-400 ml-10" onClick={e => handleDelete(index,e)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+              </button>
+            </div>
+          </td>
+        </tr>
+  )})
   return (
     <div className="w-full p-2">
         <div className="rounded-lg bg-card p-5">
@@ -301,58 +358,7 @@ function DetailLocation() {
             </tr>
           </thead>
           <tbody class="text-white">
-            {detailData.map(({id, name, type, location, state, fInfo, date, time}) => (
-            <tr>
-              <td class="sm:p-3 py-2 px-1 border-b border-gray-200">
-                <div class="flex items-center">{id}</div>
-              </td>
-              <td class="sm:p-3 py-2 px-1 border-b border-gray-200">
-                <div class="flex items-center">
-                  {name}
-                </div>
-              </td>
-              <td class="sm:p-3 py-2 px-1 border-b border-gray-200 md:table-cell hidden">
-                <div class="flex items-center">
-                  {type === "Light" ? <Image path={`https://svgshare.com/i/hj5.svg`} className="w-4 h-4 mr-2.5"/> : type === "Cooler" ? <Image path={`https://svgshare.com/i/hoA.svg`} className="w-4 h-4 mr-2.5"/>: <Image path={`https://svgshare.com/i/hnH.svg`} className="w-4 h-4 mr-2.5"/>}
-                  {type}
-                </div>
-              </td>
-              {state ? <td class="sm:p-3 py-2 px-1 border-b border-gray-200 text-green-600">On</td> : <td class="sm:p-3 py-2 px-1 border-b border-gray-200 text-red-600">Off</td>}
-              <td class="sm:p-3 py-2 px-1 border-b border-gray-200 md:table-cell hidden">
-                <div class="flex items-center">
-                  {fInfo}
-                </div>
-              </td>
-              <td class="sm:p-3 py-2 px-1 border-b border-gray-200">
-                <div class="flex items-center">
-                  <div class="sm:flex hidden flex-col">
-                    {date}
-                    <div class="text-gray-400 text-xs">{time}</div>
-                  </div>
-                  {/* <button class="w-4 h-4 inline-flex items-center justify-center text-gray-400 ml-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-                    </svg>
-                  </button> */}
-                  <EditE
-                    id={id}
-                    name={name}
-                    type={type}
-                    state={state}
-                    location={location}
-                    fInfo={fInfo}
-                  />
-                  <button class="w-4 h-4 inline-flex items-center justify-center text-gray-400 ml-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                      <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                    </svg>
-                  </button>
-                </div>
-              </td>
-            </tr>
-          ))}
+            {rows}
           </tbody>
         </table>
         </div>
@@ -538,7 +544,7 @@ function EditE({id, name, type, state, location, fInfo}) {
                       </div>
                     <div>
                         <label for="furInfo" class="block mb-2 text-sm font-medium text-gray-900">Further Info</label>
-                        <input type="furInfo" name="furInfo" id="furInfo" placeholder="20" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"/>
+                        <input type="furInfo" name="furInfo" id="furInfo" placeholder={fInfo} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"/>
                     </div>
                     <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                 </form>
